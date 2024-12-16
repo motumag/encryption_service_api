4bf7e0005be27eb788b16c4f6be29f986ff2735b
motumag <motumagishu27@gmail.com>
2024-12-16 22:55:20 +0300

    Encryption service interface has created.

--- src/main/java/com/encryption_service/service/EncryptionService.java
+++ src/main/java/com/encryption_service/service/EncryptionService.java
@@ -1,9 +1,15 @@
 package com.encryption_service.service;
 
+import com.encryption_service.dto.EncryptedRequest;
+import com.encryption_service.dto.EncryptedResponse;
+import com.encryption_service.dto.PayloadRequest;
+
 /**
  * Author: Motuma Gishu, Senior Software Engineer
  * Date: 12/16/24
  * Description: EncryptionService
  */
 public interface EncryptionService {
+    EncryptedResponse encryptPayload(PayloadRequest payload);
+    PayloadRequest decryptPayload(EncryptedRequest encryptedRequest);
 }
