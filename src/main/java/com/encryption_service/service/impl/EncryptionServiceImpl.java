f8c439e894eaf443224f27036c60be9cbf375b78
motumag <motumagishu27@gmail.com>
2024-12-16 22:56:46 +0300

    encryption service implementation has completed

--- src/main/java/com/encryption_service/service/impl/EncryptionServiceImpl.java
+++ src/main/java/com/encryption_service/service/impl/EncryptionServiceImpl.java
@@ -1,9 +1,31 @@
 package com.encryption_service.service.impl;
 
+import com.encryption_service.dto.EncryptedRequest;
+import com.encryption_service.dto.EncryptedResponse;
+import com.encryption_service.dto.PayloadRequest;
+import com.encryption_service.service.EncryptionService;
+import com.encryption_service.util.EncryptionUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 /**
  * Author: Motuma Gishu, Senior Software Engineer
  * Date: 12/16/24
  * Description: EncryptionServiceImpl
  */
-public class EncryptionServiceImpl {
+@Service
+@RequiredArgsConstructor
+public class EncryptionServiceImpl implements EncryptionService {
+
+    private final EncryptionUtil encryptionUtil;
+
+    @Override
+    public EncryptedResponse encryptPayload(PayloadRequest payload) {
+        return encryptionUtil.encrypt(payload);
+    }
+
+    @Override
+    public PayloadRequest decryptPayload(EncryptedRequest encryptedRequest) {
+        return encryptionUtil.decrypt(encryptedRequest);
+    }
 }
